17706bbb354a54ced879db8143566ddb40dcea1b
bdimaguila <briandimaguila1@gmail.com>
2025-04-07 20:06:06 +0800

    hello

--- admin/vb.php
+++ admin/vb.php
@@ -56,7 +56,7 @@ $license_status = ($confiscated == 'yes') ? 'Confiscated' : 'Not Confiscated';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>POSO Violation Receipt</title>
-    <link rel="stylesheet" href="./css/style1.css?v=1.0">
+    <link rel="stylesheet" href="./css/style1.css?v=2.0">
     <link rel="icon" href="/POSO/images/poso.png" type="image/png">
     <style>
         .impound-warning, .license-confiscated-warning {
